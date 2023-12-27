476daea459c7636fc9b00f360b9bed3d0b48b4d6
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-12-27 11:01:51 +0300

    2nd

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "flag-icon-css/css/flag-icon.min.css";
+
+// i18next ------------------------------------------------
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallback: "en",
+    debug: false,
+    supportedLngs: ["fr", "en", "ar"],
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/tranlation.json",
+    },
+    detection: {
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
+      caches: ["cookie"],
+    },
+  });
+// --------------------------------------------
+const loadingMarkup = (
+  <div className="py-4 text-center ">
+    <h2>Loading ...</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
